3a8c4f3ccf9076e9a766dfa9f7614915455028a5
Justin Wells <wellsmjustin@gmail.com>
2022-12-04 19:24:30 -0500

    Added Card Stack

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Justin Wells on 12/4/22.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 12th Doctor in Doctor who?", answer: "Peter Capaldi")
+}
--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,23 +9,37 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectagle Tapped!")
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
                 }
-            
-            Circle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Circle Tapped")
-                }
-                .allowsHitTesting(false)
+            }
         }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
     }
 }
 
